9a23dccd86de0f853f236550e4cd567f5ba447d6
Robert Snyder <r.snyder49@gmail.com>
2019-01-16 22:21:25 +0000

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,7 +4,22 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      <React.Fragment>
+      </React.Fragment>
+      <React.Fragment>
+        {directors.map(dir =>
+          <div>
+            <h3> {dir.name} </h3>
+            Movies:
+            <ul>
+              {dir.movies.map(movie =>
+                <li>{movie}</li>
+              )}
+            </ul>
+          </div>
+        )}
+      </React.Fragment>
     </div>
   );
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,23 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      <React.Fragment>
+      </React.Fragment>
+      <React.Fragment>
+        {movies.map(movie =>
+          <div>
+            <h3> {movie.title} </h3>
+            <p> {movie.time} minutes long </p>
+            Genres:
+            <ul>
+              {movie.genres.map(genre =>
+                <li>{genre}</li>
+              )}
+            </ul>
+          </div>
+        )}
+      </React.Fragment>
     </div>
   );
 };
